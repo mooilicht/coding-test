db17aebf9478cec7e8e73b488a79a37a82d16f9f
mooilicht <62236303+mooilicht@users.noreply.github.com>
2022-10-28 12:59:54 +0900

    어디에 단어가 들어갈 수 있을까

--- swea/d2/1979.py
+++ swea/d2/1979.py
@@ -1,24 +1,24 @@
 for m in range(int(input())):
-  puzzle = []
-  n, k = map(int, input().split())
-  for p in range(n):
-    puzzle.append(list(map(int, input().split())))
-  result = 0
-  for i in range(n):
-    count = 0
-    for j in range(n):
-      if puzzle[i][j] == 1:
-        count += 1
-      if puzzle[i][j] == 0 or j == n - 1:
-        if count == k:
-          result += 1
+    puzzle = []
+    n, k = map(int, input().split())
+    for p in range(n):
+        puzzle.append(list(map(int, input().split())))
+    result = 0
+    for i in range(n):
         count = 0
-    count = 0
-    for j in range(n):
-      if puzzle[j][i] == 1:
-        count += 1
-      if puzzle[j][i] == 0 or j == n - 1:
-        if count == k:
-          result += 1
+        for j in range(n):
+            if puzzle[i][j] == 1:
+                count += 1
+            if puzzle[i][j] == 0 or j == n - 1:
+                if count == k:
+                    result += 1
+                count = 0
         count = 0
-  print(f'#{m+1} {result}')
+        for j in range(n):
+            if puzzle[j][i] == 1:
+                count += 1
+            if puzzle[j][i] == 0 or j == n - 1:
+                if count == k:
+                    result += 1
+                count = 0
+    print(f"#{m+1} {result}")
