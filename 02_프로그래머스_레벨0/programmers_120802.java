c97bf630cff0eb937f14e13ea290abd0a111ed78
mooilicht <62236303+mooilicht@users.noreply.github.com>
2023-04-10 02:25:07 +0900

    프로그래머스 : 레벨 0 - 두 수의 합

---
+++ 02_프로그래머스_레벨0/programmers_120802.java
@@ -0,0 +1,11 @@
+/*
+레벨 0 두 수의 합
+
+정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 solution 함수를 완성해주세요.
+*/
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
