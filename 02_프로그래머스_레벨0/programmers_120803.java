3a64e06fe45f980a196c72050ca0ffb0f2f12301
mooilicht <62236303+mooilicht@users.noreply.github.com>
2023-04-11 02:28:24 +0900

    프로그래머스 : 레벨 0 - 두 수의 차

---
+++ 02_프로그래머스_레벨0/programmers_120803.java
@@ -0,0 +1,11 @@
+/*
+레벨 0 두 수의 차
+
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return 하도록 solution 함수를 완성해주세요.
+*/
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
