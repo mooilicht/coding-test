4949518474f359a33384f7a5384084e6c9f8a834
mooilicht <62236303+mooilicht@users.noreply.github.com>
2023-04-12 02:56:56 +0900

    프로그래머스 : 레벨 0 - 두 수의 곱

---
+++ 02_프로그래머스_레벨0/programmers_120804.java
@@ -0,0 +1,11 @@
+/*
+레벨 0 두 수의 곱
+
+정수 num1과 num2가 매개변수 주어진다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+*/
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+}
